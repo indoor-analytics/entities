e6882d0796ee4e539c9fbad14da1b4fecf8412f5
Alystrasz <raes.remy@gmail.com>
2022-03-21 11:27:38 +0100

    [feat] ReferencePath holds a collection of Points as path

--- src/ReferencePath.ts
+++ src/ReferencePath.ts
@@ -1,4 +1,4 @@
-import {LineString} from "@turf/helpers";
+import {Feature, FeatureCollection, lineString, LineString, Point} from "@turf/helpers";
 
 export default class ReferencePath {
     // Database id.
@@ -8,7 +8,7 @@ export default class ReferencePath {
     name: string;
 
     // Geometry of the reference path.
-    path: LineString;
+    path: FeatureCollection<Point, {floor: number}>;
 
 
     private constructor( data: any ) {
@@ -24,5 +24,44 @@ export default class ReferencePath {
 
     public static fromAPI(data: any): ReferencePath {
         return new ReferencePath( data );
+    }
+
+
+    /**
+     * Return the current path as a set of [lng, lat] coordinates.
+     */
+    toCoordinates (): number[][] {
+        return this.path.features.map(point => point.geometry.coordinates);
+    }
+
+    /**
+     * Returns the current object as a GeoJSON LineString object.
+     * Throws an error if the path doesn't contain at least two positions.
+     */
+    toLineString (): Feature<LineString> {
+        if (this.path.features.length <= 1)
+            throw new RangeError("A LineString must contain at least two positions.");
+        return lineString(this.toCoordinates());
+    }
+
+    /**
+     * Returns a unique identifier for this path.
+     */
+    getSourceName (): string {
+        return `path-${this.id}-${this.name}`;
+    }
+
+    /**
+     * Returns an array containing all floor numbers concerned by this path.
+     */
+    getFloors (): number[] {
+        const floors: number[] = [];
+        for (const point of this.path.features) {
+            const floor = point.properties.floor;
+            if (!floors.includes(floor)) {
+                floors.push(floor);
+            }
+        }
+        return floors;
     }
 }
